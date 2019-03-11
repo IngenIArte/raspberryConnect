f1491daad94b41cb60df4aff0838c1de3c46a5d6
SAM <ingeniartelab@gmail.com>
2019-03-11 10:34:10 -0500

    Agregando pines GPIO

--- fajaMain.py
+++ fajaMain.py
@@ -1,5 +1,15 @@
-print("hola Mundo")
+import RPi.GPIO as GPIO  
 
-print("hola cambio")
+#Para sensores se va a usar los pines 11(GPIO.17) y 12(GPIO.18)
 
-print("llega a github")
+sensorIn 	= 	17
+sensorOut 	= 	18
+
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(sensorIn, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+GPIO.setup(sensorOut, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+
+valorSensorIn = GPIO.input(sensorIn)
+
+
+print(valorSensorIn)
