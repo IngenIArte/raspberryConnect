3f4400756b769911946e25e87545d16d92711a9f
SAM <ingeniartelab@gmail.com>
2019-03-11 15:51:26 -0500

    agregando funcion de deteccion de circulos

--- fajaMain.py
+++ fajaMain.py
@@ -25,8 +25,84 @@ print(valorSensorIn)
 contaA = 0
 contaB = 0
 
+#----------------------------Funciones---------------------------------------------------------------
+def detectarCirculo(image_np):
+	global contaA
+	ipCamQueue = contaA
+	image_np = cv2.cvtColor(image_np, cv2.COLOR_BGR2GRAY)
+	print ("ingreso")
+	frameROI = image_np[44:332, 33:459]
+	#hsv = cv2.cvtColor(frameROI, cv2.COLOR_BGR2GRAY)
+	threshold = cv2.adaptiveThreshold(frameROI,255,cv2.ADAPTIVE_THRESH_GAUSSIAN_C,cv2.THRESH_BINARY_INV,11,9)
 
-#Callbacks
+	#img = cv2.medianBlur(img,5)
+	cimg = cv2.cvtColor(frameROI,cv2.COLOR_GRAY2BGR)
+	ROI = cv2.cvtColor(frameROI,cv2.COLOR_GRAY2BGR)
+	circles = cv2.HoughCircles(threshold,cv2.HOUGH_GRADIENT,1,20,
+                                param1=11, param2=20, minRadius=15, maxRadius=50)
+
+	print(type(circles))
+	try: 
+		hayCircle = len(circles[0,:])
+	except:
+		hayCircle = 0
+
+	#cv2.imwrite("imagen.png", image_np)
+	if hayCircle == 0:
+		print("es None")
+
+	else:
+		circles = np.uint16(np.around(circles))
+		print("circulos",circles)<
+		for i in circles[0,:]:
+			# draw the outer circle
+			print(i[0],i[1],i[2])
+			if i[0] > 250 and i[0] < 450 and i[1] > 40 and i[1] < 180:
+				cv2.circle(cimg,(i[0],i[1]),i[2],(0,255,0),2)
+				# draw the center of the circle
+				x = i[0]
+				y = i[1]
+				w = i[0] - 250
+				if w < 0:
+					w = 0
+				h = i[1] + 100
+				cv2.circle(cimg,(i[0],i[1]),2,(0,0,255),3)
+				cv2.rectangle(cimg,(x,y),(w,h),(255,0,0),1)
+				ROI = threshold[y:h-y+120,w:x-w+200]
+				ROIDraw = frameROI[y:h-y+120,w:x-w+200]
+
+				_, contours0, hierarchy = cv2.findContours(ROI,cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_SIMPLE)
+				area  =  []
+
+				j  = 0
+				for cnt in contours0:
+					area.append(cv2.contourArea(cnt))
+					if cv2.contourArea(cnt) > 1:
+						M = cv2.moments(cnt)
+						#print("Momento", M)
+						cx = int(M['m10']/M['m00'])
+						cy = int(M['m01']/M['m00'])
+						if cx > 20 and cx < 100:
+							#print(cx,cy)
+							j += 1
+						
+						cv2.circle(ROIDraw,(cx,cy), 5, (0,0,255), -1)
+
+				print("total", j)
+				if j < 12:
+					tamano = len(ipCamQueue)
+					cantidadX = 5 - tamano
+
+					enviaDato  = 's' + (cantidadX*'x') + ipCamQueue + 'a'
+					print("envia:", enviaDato)
+
+		#pixelValue = ROI[14,49]
+		dim = ROI.shape
+		print(dim)
+		#print("Valor de pixel", pixelValue)
+		cv2.imshow('detected circles',cimg)
+                        
+#-----------------------------Funciones para interrupcion-------------------------------------------
 def CuentaA(channel):
     global contaA
     contaA += 1
@@ -34,6 +110,7 @@ def CuentaA(channel):
     print ("Contador A: ", contaA)
     print ("Contador B: ", contaB)
     ret, frame = cap.read()
+    detectarCirculo(frame)
     cv2.imshow("Entrada", frame)
     nombreCuenta = "im" + str(contaA) + ".jpg"
     cv2.imwrite(nombreCuenta, frame)
