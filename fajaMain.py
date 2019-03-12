827ed783036bf5a8ccc2dee83543fa37f0b360bc
SAM <ingeniartelab@gmail.com>
2019-03-12 17:47:04 -0500

    se comenta (110) (122) la opcion de borrar cmd

--- fajaMain.py
+++ fajaMain.py
@@ -3,6 +3,7 @@ import RPi.GPIO as GPIO
 import threading
 import time
 import numpy as np
+
 import cv2
 cap = cv2.VideoCapture(0)
 #Para sensores se va a usar los pines 11(GPIO.17) y 12(GPIO.18)
@@ -106,7 +107,7 @@ def detectarCirculo(image_np):
 def CuentaA(channel):
     global contaA
     contaA += 1
-    os.system("clear")
+    #os.system("clear")
     print ("Contador A: ", contaA)
     print ("Contador B: ", contaB)
     ret, frame = cap.read()
@@ -118,7 +119,7 @@ def CuentaA(channel):
 def CuentaB(channel):
     global contaB
     contaB += 1
-    os.system("clear")
+    #os.system("clear")
     print ("Contador A: ", contaA)
     print ("Contador B: ", contaB)
 
